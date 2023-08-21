6482fb882e53fb64f038d717edc8495eb9c6d834
Odunayomike <micheal0olawale@gmail.com>
2023-08-21 11:36:39 +0100

    Update home-header.php

--- home-header.php
+++ home-header.php
@@ -41,9 +41,8 @@ if(mysqli_num_rows($newplan)>0){
     }  
 
 }
-
+$sub_end = new DateTime($sub_end_date);
 $currentDate = new DateTime();
-echo $currentDate;
 if($currentDate > $sub_end_date){
     $sql = "UPDATE  user SET plan = 'free', created_at ='$created_at', updated_at = NOW() WHERE user_id = $user_id ";
     if($db->query($sql) === true){
