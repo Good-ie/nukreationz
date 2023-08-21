9ed0c9345b51ddac3987d6e2d7733f1dcc532027
Odunayomike <micheal0olawale@gmail.com>
2023-08-21 14:34:21 +0100

    Update tools.php

--- tools.php
+++ tools.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['username'])) {
 }
 
 $user_id =  $_SESSION["user_id"];
-$sql = "SELECT plan FROM user WHERE user_id = $user_id";
+$sql = "SELECT * FROM user WHERE user_id = $user_id";
 $plan = mysqli_query($db, $sql);
 if (mysqli_num_rows($plan) > 0) {
 
