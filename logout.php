418d61bc881790b563b76889c23172c4b2b405e2
Odunayomike <micheal0olawale@gmail.com>
2023-08-22 13:00:18 +0100

    making file structure adjustment

--- logout.php
+++ logout.php
@@ -1,7 +1,13 @@
-<?php
-session_start();
-session_destroy();
-unset($_SESSION['username']);
-
-header("Location:index.php");
+<?php
+
+session_start();
+
+session_destroy();
+
+unset($_SESSION['username']);
+
+
+
+header("Location: ./");
+
 ?>
